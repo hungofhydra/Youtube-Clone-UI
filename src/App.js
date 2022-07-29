d52be5aac880ff6e2d6758cbf707e144e774e9e5
Nguyen Tan Hung <hungofhydraforwork@gmail.com>
2022-07-29 09:31:01 +0700

    Add theme function. Users can change between light and dark mode

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import styled from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
 import Menu from "./components/Menu";
 import Navbar from "./components/Navbar";
 import React from "react";
+import { darkTheme, lightTheme } from "./utils/Theme";
 const Container = styled.div`
   display: flex;
 `;
 const Main = styled.div`
   flex: 7;
+  background-color: ${({ theme }) => theme.background};
 `;
 const Wrapper = styled.div``;
 function App() {
+  const [dark, setDark] = React.useState(false);
   return (
-    <Container>
-      <Menu />
-      <Main>
-        <Navbar />
-        <Wrapper>Video cards
-
-          
-        </Wrapper>
-
-      </Main>
-    </Container>
+    <ThemeProvider theme={dark ? darkTheme : lightTheme}>
+      <Container>
+        <Menu dark={dark} setDark={setDark} />
+        <Main>
+          <Navbar />
+          <Wrapper>Video cards</Wrapper>
+        </Main>
+      </Container>
+    </ThemeProvider>
   );
 }
 
---
+++ src/utils/Theme.js
@@ -0,0 +1,17 @@
+const darkTheme = {
+  background: "#181818",
+  backgroundLighter: "#202020",
+  text: "white",
+  textSoft: "#aaaaaa",
+  soft: "#373737",
+};
+
+const lightTheme = {
+  background: "#f9f9f9",
+  backgroundLighter: "white",
+  text: "black",
+  textSoft: "#606060",
+  soft: "#f5f5f5",
+};
+
+export { darkTheme, lightTheme };
